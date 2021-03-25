2b5c0c793755dcdb4c9edad9e0f379e546e3fbaa
andre-pires2 <andre.pires2@fatec.sp.gov.br>
2021-03-25 02:41:11 +0000

    Adicionando comentários ao código

--- Pratica_01/app/src/main/java/fatec/labvi/Calculadora.java
+++ Pratica_01/app/src/main/java/fatec/labvi/Calculadora.java
@@ -3,6 +3,7 @@ package fatec.labvi;
 public class Calculadora {
     public static void main(String[] args) {}
 
+    //Método de subtração da calculadora
     public double subtrair(double x, double y) {
         return x-y;
     }
--- Pratica_01/app/src/test/java/fatec/labvi/CalculadoraTest.java
+++ Pratica_01/app/src/test/java/fatec/labvi/CalculadoraTest.java
@@ -7,15 +7,18 @@ import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
 
+    //Declaração comum entre os testes
     Calculadora calculadora = new Calculadora();
     double a = 10.0;
     double b = 5.0;
 
+    //Teste que garante o caso de sucesso do método subtrair da classe Calculadora
     @Test
     public void testCalculadoraSubtracaoSucesso() {
         assertEquals(5.0,calculadora.subtrair(a,b));
     }
 
+    //Teste que garante o caso de falha do método subtrair da classe Calculadora
     @Test
     public void testCalculadoraSubtracaoFalha() {
         assertNotEquals(0,calculadora.subtrair(a,b));
