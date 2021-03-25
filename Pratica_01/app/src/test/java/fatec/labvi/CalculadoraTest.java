9b27bcf2851636575030c9c63b55ae5a0ad00a3d
andre-pires2 <andre.pires2@fatec.sp.gov.br>
2021-03-25 01:27:06 +0000

    TDD fase vermelha

--- Pratica_01/app/src/test/java/fatec/labvi/CalculadoraTest.java
+++ Pratica_01/app/src/test/java/fatec/labvi/CalculadoraTest.java
@@ -5,4 +5,14 @@ import static org.junit.Assert.*;
 
 public class CalculadoraTest {
 
+    @Before
+    Calculadora calculadora = new Calculadora();
+
+    @Test
+    public void testCalculadoraSubtracao() {
+        int a = 10;
+        int b = 5;
+
+        assertEquals(5,calculadora.subtrair(a,b));
+    }
 }
