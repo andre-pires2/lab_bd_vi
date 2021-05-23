d682c7b501e7e3fecf926b4b524e755070fbb17e
andre-pires2 <andre.pires2@fatec.sp.gov.br>
2021-05-23 21:35:08 +0000

    Fase azul TDD

--- Pratica_02/app/src/main/java/fatec/labvi/Calculadora.java
+++ Pratica_02/app/src/main/java/fatec/labvi/Calculadora.java
@@ -3,9 +3,9 @@ package fatec.labvi;
 public class Calculadora {
     public static void main(String[] args) {}
 
+    //Metodo de subtração da calculadora
     public float subtrair(float x, float y)
     {
-        float resultado = x-y;
-        return resultado;
+        return x-y;
     }
 }
