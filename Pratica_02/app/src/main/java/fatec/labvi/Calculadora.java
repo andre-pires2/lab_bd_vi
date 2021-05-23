bfe38944f52df59519a24076b19c474d22eb8d3e
andre-pires2 <andre.pires2@fatec.sp.gov.br>
2021-05-23 21:31:44 +0000

    Fase vermelha TDD

--- Pratica_02/app/src/main/java/fatec/labvi/Calculadora.java
+++ Pratica_02/app/src/main/java/fatec/labvi/Calculadora.java
@@ -3,4 +3,8 @@ package fatec.labvi;
 public class Calculadora {
     public static void main(String[] args) {}
 
+    public long subtrair()
+    {
+        return 1L;
+    }
 }
--- Pratica_02/app/src/test/java/fatec/labvi/CalculadoraTest.java
+++ Pratica_02/app/src/test/java/fatec/labvi/CalculadoraTest.java
@@ -6,5 +6,37 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
+    
+    //Declaração comum entre os testes
+    Calculadora calculadora = new Calculadora();
 
+    //Teste com valor 0
+    @Test
+    public void subtracaoComZero() {
+        assertEquals(0f,calculadora.subtrair(0f, 0f), 0.01f);
+    }
+
+    //Teste com valor proximo ao valor limite positivo inferior
+    @Test
+    public void subtracaoComValorLimitePositivoInferior() {
+        assertEquals(0.01f,calculadora.subtrair(0.11f, 0.1f), 0.01f);
+    }
+
+    //Teste com valor proximo ao valor limite positivo superior
+    @Test
+    public void subtracaoComValorLimitePositivoSuperior() {
+        assertEquals(0.01f,calculadora.subtrair(99.99f,99.98f), 0.01f);
+    }
+
+    //Teste com valor proximo ao valor limite negativo inferior
+    @Test
+    public void subtracaoComValorLimiteNegativoInferior() {
+        assertEquals(-0.01f,calculadora.subtrair(-0.11f,-0.1f), 0.01f);
+    }
+
+    //Teste com valor proximo ao valor limite negativo superior
+    @Test
+    public void subtracaoComValorLimiteNegativoSuperior() {
+        assertEquals(-0.01f,calculadora.subtrair(-99.99f,-99.98f), 0.01f);
+    }
 }
